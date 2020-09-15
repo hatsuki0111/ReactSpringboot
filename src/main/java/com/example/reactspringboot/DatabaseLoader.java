01cd439343f2b39fba733f68e25f8ee1227c756f
hatsuki0111 <cist.b217.h.saitou@gmail.com>
2020-09-16 00:21:07 +0900

    fix

--- src/main/java/com/example/reactspringboot/DatabaseLoader.java
+++ src/main/java/com/example/reactspringboot/DatabaseLoader.java
@@ -3,14 +3,22 @@ package com.example.reactspringboot;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
 
 @Component
-public class DatabaseLoader implements CommandLineRunner {
+public class DatabaseLoader implements CommandLineRunner{
+
 
     private final IEmployeeRepository employeeRepository;
 
+
+
     @Autowired
     public DatabaseLoader(IEmployeeRepository employeeRepository) {
+
         this.employeeRepository = employeeRepository;
     }
 
@@ -18,4 +26,5 @@ public class DatabaseLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
         this.employeeRepository.save(new Employee("Yamada", "Tarou", "engineer"));
     }
+
 }
--- src/main/java/com/example/reactspringboot/IEmployeeRepository.java
+++ src/main/java/com/example/reactspringboot/IEmployeeRepository.java
@@ -1,6 +1,8 @@
 package com.example.reactspringboot;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
 
 public interface IEmployeeRepository extends CrudRepository<Employee, Long> {
 
